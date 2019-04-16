c12bdd345f178654a8f7648ccd3c8464a0892256
KenTheCoder <Kenthekool88@users.noreply.github.com>
2019-04-16 09:12:10 -0400

    Can now retain instance state.

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,20 +1,94 @@
 package com.example.shoppinglist;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final int TEXT_REQUEST = 1;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        textView1 = findViewById(R.id.first_item);
+        textView2 = findViewById(R.id.second_item);
+        textView3 = findViewById(R.id.third_item);
+        textView4 = findViewById(R.id.fourth_item);
+        textView5 = findViewById(R.id.fifth_item);
+        textView6 = findViewById(R.id.sixth_item);
+        textView7 = findViewById(R.id.seventh_item);
+        if (textView1.getVisibility() == View.VISIBLE) {
+            outState.putString("first_item", textView1.getText().toString());
+        }
+        if (textView2.getVisibility() == View.VISIBLE) {
+            outState.putString("second_item", textView2.getText().toString());
+        }
+        if (textView3.getVisibility() == View.VISIBLE) {
+            outState.putString("third_item", textView3.getText().toString());
+        }
+        if (textView4.getVisibility() == View.VISIBLE) {
+            outState.putString("fourth_item", textView4.getText().toString());
+        }
+        if (textView5.getVisibility() == View.VISIBLE) {
+            outState.putString("fifth_item", textView5.getText().toString());
+        }
+        if (textView6.getVisibility() == View.VISIBLE) {
+            outState.putString("sixth_item", textView6.getText().toString());
+        }
+        if (textView7.getVisibility() == View.VISIBLE) {
+            outState.putString("seventh_item", textView7.getText().toString());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView1 = findViewById(R.id.first_item);
+        textView2 = findViewById(R.id.second_item);
+        textView3 = findViewById(R.id.third_item);
+        textView4 = findViewById(R.id.fourth_item);
+        textView5 = findViewById(R.id.fifth_item);
+        textView6 = findViewById(R.id.sixth_item);
+        textView7 = findViewById(R.id.seventh_item);
+        if (savedInstanceState != null) {
+            textView1.setVisibility(View.VISIBLE);
+            textView2.setVisibility(View.VISIBLE);
+            textView3.setVisibility(View.VISIBLE);
+            textView4.setVisibility(View.VISIBLE);
+            textView5.setVisibility(View.VISIBLE);
+            textView6.setVisibility(View.VISIBLE);
+            textView7.setVisibility(View.VISIBLE);
+            textView1.setText(savedInstanceState.getString("first_item"));
+            textView2.setText(savedInstanceState.getString("second_item"));
+            textView3.setText(savedInstanceState.getString("third_item"));
+            textView4.setText(savedInstanceState.getString("fourth_item"));
+            textView5.setText(savedInstanceState.getString("fifth_item"));
+            textView6.setText(savedInstanceState.getString("sixth_item"));
+            textView7.setText(savedInstanceState.getString("seventh_item"));
+            textView1.setVisibility(View.VISIBLE);
+            textView2.setVisibility(View.VISIBLE);
+            textView3.setVisibility(View.VISIBLE);
+            textView4.setVisibility(View.VISIBLE);
+            textView5.setVisibility(View.VISIBLE);
+            textView6.setVisibility(View.VISIBLE);
+            textView7.setVisibility(View.VISIBLE);
+        }
     }
 
     public void launchUsualItems(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, UsualItems.class);
         startActivityForResult(intent, TEXT_REQUEST);
     }
@@ -22,16 +96,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        textView1 = findViewById(R.id.first_item);
+        textView2 = findViewById(R.id.second_item);
+        textView3 = findViewById(R.id.third_item);
+        textView4 = findViewById(R.id.fourth_item);
+        textView5 = findViewById(R.id.fifth_item);
+        textView6 = findViewById(R.id.sixth_item);
+        textView7 = findViewById(R.id.seventh_item);
         if (requestCode == TEXT_REQUEST) {
             if (resultCode == RESULT_OK) {
-                TextView textView1 = findViewById(R.id.first_item);
-                TextView textView2 = findViewById(R.id.second_item);
-                TextView textView3 = findViewById(R.id.third_item);
-                TextView textView4 = findViewById(R.id.fourth_item);
-                TextView textView5 = findViewById(R.id.fifth_item);
-                TextView textView6 = findViewById(R.id.sixth_item);
-                TextView textView7 = findViewById(R.id.seventh_item);
                 textView1.setText(UsualItems.message1);
                 textView2.setText(UsualItems.message2);
                 textView3.setText(UsualItems.message3);
--- app/src/main/java/com/example/shoppinglist/UsualItems.java
+++ app/src/main/java/com/example/shoppinglist/UsualItems.java
@@ -2,11 +2,13 @@ package com.example.shoppinglist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class UsualItems extends AppCompatActivity {
-
+    private static final String LOG_TAG
+            = UsualItems.class.getSimpleName();
     public static String message1;
     public static String message2;
     public static String message3;
@@ -16,55 +18,61 @@ public class UsualItems extends AppCompatActivity {
     public static String message7;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usual_items);
     }
 
     public void returnChoice1(View view) {
-        ImageButton buttonSend1 = (ImageButton) findViewById(R.id.imageButton1);
+        ImageButton buttonSend1 = findViewById(R.id.imageButton1);
         message1 = buttonSend1.getContentDescription().toString();
         setResult(RESULT_OK);
         finish();
     }
 
     public void returnChoice2(View view) {
-        ImageButton buttonSend2 = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton buttonSend2 = findViewById(R.id.imageButton2);
         message2 = buttonSend2.getContentDescription().toString();
         setResult(RESULT_OK);
         finish();
     }
 
     public void returnChoice3(View view) {
-        ImageButton buttonSend3 = (ImageButton) findViewById(R.id.imageButton3);
+        ImageButton buttonSend3 = findViewById(R.id.imageButton3);
         message3 = buttonSend3.getContentDescription().toString();
         setResult(RESULT_OK);
         finish();
     }
 
     public void returnChoice4(View view) {
-        ImageButton buttonSend4 = (ImageButton) findViewById(R.id.imageButton4);
+        ImageButton buttonSend4 = findViewById(R.id.imageButton4);
         message4 = buttonSend4.getContentDescription().toString();
         setResult(RESULT_OK);
         finish();
     }
 
     public void returnChoice5(View view) {
-        ImageButton buttonSend5 = (ImageButton) findViewById(R.id.imageButton5);
+        ImageButton buttonSend5 = findViewById(R.id.imageButton5);
         message5 = buttonSend5.getContentDescription().toString();
         setResult(RESULT_OK);
         finish();
     }
 
     public void returnChoice6(View view) {
-        ImageButton buttonSend6 = (ImageButton) findViewById(R.id.imageButton6);
+        ImageButton buttonSend6 = findViewById(R.id.imageButton6);
         message6 = buttonSend6.getContentDescription().toString();
         setResult(RESULT_OK);
         finish();
     }
 
     public void returnChoice7(View view) {
-        ImageButton buttonSend7 = (ImageButton) findViewById(R.id.imageButton7);
+        ImageButton buttonSend7 = findViewById(R.id.imageButton7);
         message7 = buttonSend7.getContentDescription().toString();
         setResult(RESULT_OK);
         finish();
